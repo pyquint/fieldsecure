90acf5da8ba017194e9e40feffe26a1b5031c8ae
pyquint <83896843+pyquint@users.noreply.github.com>
2025-03-16 15:20:00 +0800

    done with hill cipher - encryption - api endpoint - learn section

---
+++ app/types.py
@@ -0,0 +1,14 @@
+from typing import Tuple, TypeVar
+
+import numpy as np
+
+T = TypeVar("T", bound=np.generic, covariant=True)
+
+Vector = np.ndarray[Tuple[int], np.dtype[T]]
+ColumnVector = np.ndarray[Tuple[Tuple[int]], np.dtype[T]]
+
+Matrix = np.ndarray[Tuple[int, int], np.dtype[T]]
+Tensor = np.ndarray[Tuple[int, ...], np.dtype[T]]
+
+
+# https://stackoverflow.com/a/75499982
